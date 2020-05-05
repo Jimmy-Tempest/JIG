57bc4e208ec2137903ac4474858793894b5fa742
Jimmy_Tempest <53175779+Jimmy-Tempest@users.noreply.github.com>
2020-05-05 13:37:21 +0700

    Delete bot.js

--- bot.js
+++
@@ -1,9 +0,0 @@
-const Discord = require('discord.js');
-const YTDL = require('ytdl-core');
-const bot = new Discord.Client();
-
-bot.on('ready', () =>{
-    console.log('This bot is online!');
-})
-
-cleint.login(process.env.BOT_TOKEN);
